7d2e2e175e37e18e2b59f6cb55f8566a5d4692db
Jodi Bell <jodi@jodibell.com>
2019-03-03 11:40:55 -0500

    removed orm file

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -11,7 +11,7 @@ router.get("/", function(req, res) {
 
 router.get("/burgers", function(req, res) {
   // express callback response by calling burger.selectAllBurger
-  burger.all(function(burgerData) {
+  burger.findall(function(burgerData) {
     // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
     res.render("index", { burger_data: burgerData });
   });
