207b0e40ae697d9ef69f5743ae31639a31f7b23a
Jodi Bell <jodi@jodibell.com>
2019-03-14 17:01:43 -0400

    devour button not redirecting but otherwise ok

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -5,22 +5,22 @@ var db = require("../models");
 var burger = db.Burger;
 
 // get route -> index
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
   res.redirect("/burgers");
 });
 
-router.get("/burgers", function(req, res) {
+router.get("/burgers", function (req, res) {
   // express callback response by calling burger.selectAllBurger
-  burger.findAll().then(burgerData=> {
+  burger.findAll().then(burgerData => {
     res.render("index", { burger_data: burgerData });
   });
-    // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
+  // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
 });
 
 // post route -> back to index
-router.post("/burgers/create", function(req, res) {
+router.post("/burgers/create", function (req, res) {
   // takes the request object using it as input for burger.addBurger
-  burger.create(req.body).then(function(result) {
+  burger.create(req.body).then(function (result) {
     // wrapper for orm.js that using MySQL insert callback will return a log to console,
     // render back to index with handle
     res.redirect("/");
@@ -28,13 +28,13 @@ router.post("/burgers/create", function(req, res) {
 });
 
 // put route -> back to index
-router.put("/burgers/:id", function(req, res) {
-  burger.update(req.params.id, function(result) {
+router.put("/burgers/:id", function (req, res) {
+  burger.update({devoured:1}, { where: { id: req.params.id } }, function (result) {
     // wrapper for orm.js that using MySQL update callback will return a log to console,
     // render back to index with handle
     console.log(result);
     // Send back response and let page reload from .then in Ajax
-    res.sendStatus(200);
+    res.send(result);
   });
 });
 
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
       url: "/burgers/" + burger_id
     }).then(function(data) {
       // reload page to display devoured burger in proper column
-      location.reload();
+      location.reload(true);
     });
 
   });
