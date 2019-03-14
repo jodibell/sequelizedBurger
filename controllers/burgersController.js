c8cdae2fbb121411e3bf6ce594812c268330c86d
Jodi Bell <jodi@jodibell.com>
2019-03-14 16:44:07 -0400

    worked on burgerController

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -11,19 +11,18 @@ router.get("/", function(req, res) {
 
 router.get("/burgers", function(req, res) {
   // express callback response by calling burger.selectAllBurger
-  burger.findall(function(burgerData) {
-    // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
+  burger.findAll().then(burgerData=> {
     res.render("index", { burger_data: burgerData });
   });
+    // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
 });
 
 // post route -> back to index
 router.post("/burgers/create", function(req, res) {
   // takes the request object using it as input for burger.addBurger
-  burger.create(req.body.burger_name, function(result) {
+  burger.create(req.body).then(function(result) {
     // wrapper for orm.js that using MySQL insert callback will return a log to console,
     // render back to index with handle
-    console.log(result);
     res.redirect("/");
   });
 });
